21c1499ad0efc07810d202aec56aa8464c0ae90a
Rvickie <manlikerain@gmail.com>
2023-03-19 09:08:41 +0000

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints the phrase with proper grammer,
+ *but the outcome is a piece of art,
  *Return: Always 0 (Success)
  */
 int main(void)
