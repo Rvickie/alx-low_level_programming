b0a9636878f63cb30fd9939afae38e8282aa5cf0
Rvickie <manlikerain@gmail.com>
2023-03-19 08:50:01 +0000

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
-  * main - this is important function
- *
- * Return: always 0
+ * main - prints the phrase "programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
-
