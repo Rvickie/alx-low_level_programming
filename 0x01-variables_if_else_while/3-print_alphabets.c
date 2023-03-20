8a73ee0c580d9b1002ddc700202bd424a0ef6d8f
Rvickie <manlikerain@gmail.com>
2023-03-20 17:15:31 +0000

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  * main - main function
  *
- * Rturn: Always 0 (Success)
+ * Return: always 0
  */
 
 int main(void)
@@ -11,8 +11,9 @@ int main(void)
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-	putchar(i);
+		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
