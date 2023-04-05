8cfc33bdd75f51228dbf24c5215b976376063f6a
Rvickie <manlikerain@gmail.com>
2023-04-05 06:53:40 +0000

    spn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,18 +10,18 @@
 unsigned int _strspn(char *accept, char *s)
 {
 	unsigned int n = 0;
-	int j;
+	int r;
 
 	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		for (r = 0; accept[r]; r++)
 		{
-			if (*s == accept[j])
+			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if (accept[j + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
