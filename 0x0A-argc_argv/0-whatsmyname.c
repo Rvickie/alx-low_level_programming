760e42ee386b79d40d447f2c89be8e242712eb9b
Rvickie <manlikerain@gmail.com>
2023-04-09 04:51:11 +0000

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,8 @@
 /**
  * main - Function that prints the name
  * of the programme
- * @argc: Input
- * @argv: Input
+ * @argc: Number of arguements
+ * @argv: Array of arguements
  *
  * Return: Always 0
  */
