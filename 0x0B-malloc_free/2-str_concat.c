91a0237372254be02a6e5ac815faae84ee6edd26
Rvickie <manlikerain@gmail.com>
2023-04-11 22:13:20 +0000

    con

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,8 +4,8 @@
 /**
  * str_concat - Function that get ends of input and then
  * add together for size
- * @s1: input
- * @s2: input
+ * @s1: input one to concat
+ * @s2: input two to concat
  *
  * Return: concat of s1 and s2
  */
