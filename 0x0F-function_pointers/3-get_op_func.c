5af1ba127fcf8c13f4e993b91b6c1a048cb82501
Rvickie <manlikerain@gmail.com>
2023-04-21 06:56:21 +0000

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - Function that selects the correct function
- *        to perform the operation asked by the user
- * @s: input
+ * get_op_func - Selects the correct function to perform
+ *        the operation asked by the user.
+ * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
- * to the operator given as a parameter
+ *     to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -20,10 +20,10 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL},
 	};
 
-	int j = 0;
+	int i = 0;
 
-	while (ops[j].op != NULL && *(ops[j].op) != *s)
-		j++;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
 	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,11 +7,11 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - Function thet returns the sum of two numbers
- * @a: The first number
- * @b: The second number
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The sum of a and b
+ * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
@@ -19,33 +19,31 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - Function that returns the difference of two numbers
- * @a: The first number
- * @b: The second number
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The difference of a and b
+ * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - Function that returns the product of two numbers
- * @a: The first number
- * @b: The second number
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The product of a and b
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - Function that returns the division of two numbers
- * @a: The first number
- * @b: The second number
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
  * Return: The quotient of a and b.
  */
@@ -53,14 +51,12 @@ int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod - Function that returns the remainder of the
- * division of two numbers
- * @a: The first number
- * @b: The second number
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The remainder of the division of a by b
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
